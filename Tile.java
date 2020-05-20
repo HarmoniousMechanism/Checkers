5d663e582dd0dd436a579c0277695934b74db961
Harmonious <harmoniousmechanism@gmail.com>
2020-05-20 13:50:55 -0400

    Completed the Tile Class, Piece class needs logic checked for attacking a piece on move, Player's non Move methods completed, Game's non Move methods completed.

--- Tile.java
+++
@@ -1,8 +0,0 @@
-
-public class Tile {
-	
-	public Tile(int row, int column) {}
-	public Piece getOccupant(){ return null; }
-	public void setOccupant(Piece newOccupant){}
-	public boolean isOccupied() { return false;	}
-}
